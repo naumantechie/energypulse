712855ae3dcb0c7119f29c2c3cf28e40bf2f1d68
naumantechie <naumannazir448@gmail.com>
2026-05-14 14:33:13 -0400

    Global types

---
+++ types/index.ts
@@ -0,0 +1,48 @@
+export interface Country {
+    id: string;
+    code: string;
+    name: string;
+    continent: string;
+    population: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface EnergyRecord {
+    id: string;
+    countryId: string;
+    year: number;
+    solarGwh: number;
+    windGwh: number;
+    hydroGwh: number;
+    nuclearGwh: number;
+    fossilGwh: number;
+    totalGwh: number;
+    renewablePercent: number;
+    carbonIntensity: number;
+    createdAt: string;
+}
+
+export interface liveGrid {
+    id: string;
+    countryCode: string;
+    carbonIntensity: number;
+    renewablePercent: number;
+    fossilPercent: number;
+    syncedAt: string;
+}
+
+export interface GlobalStats {
+    year: number;
+    _sum: {
+        solarGwn: number;
+        windGwh: number;
+        hydroGwh: number;
+        fossilGwh: number;
+        totalGwh: number;
+    };
+}
+
+export interface CountryWithEnergy extends Country {
+    energyRecods: EnergyRecord;
+}
